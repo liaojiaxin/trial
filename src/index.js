a5677c92ebd56d45e5853a98fc2b9975f1e49588
liaojiaxin <ljx11606@163.com>
2018-07-25 20:44:37 +0800

    third trial, add all files about demo and update index.html

---
+++ src/index.js
@@ -0,0 +1,15 @@
+﻿import _ from "lodash";
+
+function component(){
+	// 生成一个div元素
+	var divElement = document.createElement("div");
+	
+	// 写入内容
+	// Lodash, now imported by this script
+	divElement.innerHTML = _.join(["Hello", "webpack"], " ");
+	
+	return divElement;
+}
+
+document.body.appendChild(component());
+
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+﻿var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
+
+
